a9eb53a169c2588beafec19d228d90733183d810
Jianfeng Zhang <swordfeng123@gmail.com>
2017-09-30 21:34:16 +0800

    refine plugin manager

--- plugin_manager.py
+++ plugin_manager.py
@@ -4,51 +4,61 @@ import sys
 import os
 from util import *
 from ns import *
+import db
 import plugins
 
 #sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 logger = logging.getLogger('plugin_manager')
 
-loaded_plugins = {}
-
 assert_plugin_name = assert_on_re('plugin name', r'^[A-Za-z0-9\-_]+$')
 
-def load_plugin(name: str):
+loaded_plugins = {}
+enabled_plugins = db.get_json('pm')
+if enabled_plugins is None:
+    enabled_plugins = set()
+
+def load_plugin(name):
     assert_plugin_name(name)
     if name in loaded_plugins:
-        mod = loaded_plugins[name]
-        try:
-            mod.finalize()
-        except:
-            logger.exception(f'Error raised while unloading {name}')
-    else:
+        return
+    try:
         mod = import_module(f'.{name}', 'plugins')
         loaded_plugins[name] = mod
-    try:
         reload(mod) # refresh module if changed
         mod.initialize()
     except:
         del(loaded_plugins[name])
-        # todo: handle exception (disable&restart program?)
+        # todo: handle exception
         raise
 
-def unload_plugin(name: str):
+def unload_plugin(name):
     assert_plugin_name(name)
-    if name in loaded_plugins:
+    if name not in loaded_plugins:
+        return
+    try:
         mod = loaded_plugins[name]
         mod.finalize()
         del(loaded_plugins[name])
+    except:
+        del(loaded_plugins[name])
+        # todo: handle exception
+        raise
+
+def reload_plugin(name):
+    load(name)
+    unload(name)
 
 def list_plugin():
-    pass
+    return {'loaded': loaded_plugins.keys(), 'enabled': list(enabled_plugins)}
 
 def enable_plugin(name: str):
-    pass
+    enabled_plugins.add(name)
+    db.put_json(list(enabled_plugins))
 
 def disable_plugin(name: str):
-    pass
-
+    enabled_plugins.discard(name)
+    db.put_json(list(enabled_plugins))
 
 class PluginManager:
     def ident():
@@ -60,7 +70,19 @@ class PluginManager:
         else:
             return mod.query(ids[1:])
     def load(name):
-        load_plugin(name)
+        return load_plugin(name)
     def unload(name):
-        unload_plugin(name)
+        return unload_plugin(name)
+    def reload(name):
+        return reload_plugin(name)
+    def enable(name):
+        return enable_plugin(name)
+    def disable(name):
+        return disable_plugin(name)
+    def list(name):
+        return list_plugin(name)
 register_root(PluginManager)
+
+for plugin_name in enabled_plugins:
+    load(plugin_name)
+
