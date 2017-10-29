f4f678c19a099e5e20bed32e69357f76bf1767bd
Jianfeng Zhang <swordfeng123@gmail.com>
2017-10-29 20:41:34 +0800

    add convenient object query

--- core.py
+++ core.py
@@ -1,5 +1,5 @@
 from framework import Channel, Message, User
-from ns import query_object, register_root, unregister_root, cache_object, uncache_object
+from ns import query_object, register_root, unregister_root, cache_object, uncache_object, _
 from util import *
 from par import *
 
